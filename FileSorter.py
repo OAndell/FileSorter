a069ea3ec8cc0e13194bc0e674e9fedf60375184
Oscar <Oscar@andell.eu>
2016-09-01 13:58:49 +0200

    Initial commit

---
+++ FileSorter.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+def createdir(newDir):
+    if not os.path.isdir(newDir):
+        os.makedirs(newDir)
+    return newDir;
+def removeEmpty(directories):
+    for dir in directories:
+        if (len(os.listdir(dir)) == 0):
+            shutil.rmtree(dir)
+    return;
+
+print("Sorting Files...")
+currentDir = os.getcwd()
+videoDir = createdir(currentDir + "/Videos")
+pictureDir = createdir(currentDir + "/Pictures")
+audioDir = createdir(currentDir + "/Audio")
+programDir = createdir(currentDir + "/Programs")
+documentDir = createdir(currentDir + "/Documents")
+archiveDir = createdir(currentDir + "/Archived")
+torrentDir = createdir(currentDir + "/Torrents")
+directories = [videoDir, pictureDir, audioDir, programDir, documentDir, archiveDir, torrentDir]
+files = os.listdir() #Get files in folder
+for i in files: #Sort files in specific folders
+    if(i.lower().endswith(".png") or i.lower().endswith(".jpg") or i.lower().endswith(".gif")):     #PICTURES
+        shutil.move(currentDir + "/" + i, pictureDir + "/" + i)
+    if (i.lower().endswith(".exe")):                                                                 #PROGRAMS
+        shutil.move(currentDir + "/" + i, programDir + "/" + i)
+    if (i.lower().endswith(".mp3")  or i.lower().endswith(".waw")):                                    #AUDIO
+        shutil.move(currentDir + "/" + i, audioDir + "/" + i)
+    if (i.lower().endswith(".pdf") or i.lower().endswith(".txt")  or i.lower().endswith(".html")
+        or i.lower().endswith(".docx") or i.lower().endswith(".odt") or i.lower().endswith(".doc") ):    #DOCUMENTS
+        shutil.move(currentDir + "/" + i, documentDir + "/" + i)
+    if (i.lower().endswith(".mp4") or i.lower().endswith(".wmw") or i.lower().endswith(".avi")):          # Video
+        shutil.move(currentDir + "/" + i, programDir + "/" + i)
+    if (i.lower().endswith(".zip") or i.lower().endswith(".rar") or i.lower().endswith(".jar")
+        or i.lower().endswith(".gz") or i.lower().endswith(".iso")):                                     # ARCHIVE
+        shutil.move(currentDir + "/" + i, archiveDir + "/" + i)
+    if (i.lower().endswith(".torrent")):                                                               # torrents
+        shutil.move(currentDir + "/" + i, torrentDir + "/" + i)
+removeEmpty(directories) #Removes unused folders
+print("Sorting Complete")
